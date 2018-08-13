70c27899b7516f7e46dd6c8aed39c484dd8287b4
Mark Headley <1753393+mtheadley@users.noreply.github.com>
2018-08-13 10:23:01 -0400

    setup and starting problem 8

---
+++ golang/problem008.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	dat, err := ioutil.ReadFile("./data/info008.txt")
+	check(err)
+	fmt.Print(string(dat))
+}
---
+++ golang/setup.sh
@@ -0,0 +1,1 @@
+export GOPATH=/Users/mark/Development/Euler/golang

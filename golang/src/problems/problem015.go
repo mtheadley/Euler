db241c8661f27bdb207cd92ccdef08621e64993c
Mark Headley <1753393+mtheadley@users.noreply.github.com>
2018-09-07 13:11:14 -0400

    starting problem 15

--- golang/src/problems/problem015.go
+++ golang/src/problems/problem015.go
@@ -9,5 +9,6 @@ func Problem015() string {
 	// (40)
 	// (20)
 	//40 choose 20 :: combinatorics
+	//https://www.hackmath.net/en/calculator/combinations-and-permutations?n=40&k=20&order=0&repeat=0
 	return fmt.Sprintf("NOPE")
 }
